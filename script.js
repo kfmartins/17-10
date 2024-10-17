d392d2590fd85cb8f0b81d3b521cde361298627b
Kauan Martins <127758636+kfmartins@users.noreply.github.com>
2024-10-17 10:08:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function
+appendToDisplay (value) {
+    document.getElementById('display').value += value;
+}
+
+function
+clearDisplay () {
+    document.getElementById('display').value = '';
+}
+
+function
+deleteLast () {
+    let currentvalue = document.getElementById('display').value;
+
+    document.getElementById('display').value = currentValue.slice(0, -1);
+}
+
+function calculateResult() {
+    let currentValue = document.getElementById('display').value; try{
+    document.getElementById('display').value = eval(currentValue);
+}catch (error) {
+    document.getElementById('display').value = 'Error';
+
+}
+}
+
+
